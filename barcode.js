90667ece68a5db4b35fac79b1d52804ddc05be2a
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-03-04 11:53:49 +0300

    [[

--- barcode.js
+++ barcode.js
@@ -1,11 +1,4 @@
-barcode.config.start = 0.1;
-barcode.config.end = 0.9;
-barcode.config.video = '#barcodevideo';
-barcode.config.canvas = '#barcodecanvas';
-barcode.config.canvasg = '#barcodecanvasg';
-barcode.setHandler(function(barcode) {
-    console.log(barcode);
-});
+
 
 barcode.init();
 
@@ -67,6 +60,15 @@ var barcode = function() {
 		canvas: '',
 		canvasg: ''
 	}
+	
+	barcode.config.start = 0.1;
+	barcode.config.end = 0.9;
+	barcode.config.video = '#barcodevideo';
+	barcode.config.canvas = '#barcodecanvas';
+	barcode.config.canvasg = '#barcodecanvasg';
+	barcode.setHandler(function(barcode) {
+	    console.log(barcode);
+	});
 
 	function init() {
 
